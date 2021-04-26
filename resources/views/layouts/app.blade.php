5a0a2083770a4eb96cbda534aee691d0a7e3c150
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-04-26 22:02:57 +0300

    publishers nav bar added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,6 +73,19 @@
                                </div>
                            </li>
                            <li class="nav-item dropdown">
+                           <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                   Publishers
+                               </a>
+                               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                   <a class="dropdown-item" href="{{ route('publisher.index') }}">
+                                       Publishers List
+                                   </a>
+                                   <a class="dropdown-item" href="{{ route('publisher.create') }}">
+                                       New Publisher
+                                   </a>
+                               </div>
+                           </li>
+                           <li class="nav-item dropdown">
                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                    Books
                                </a>
