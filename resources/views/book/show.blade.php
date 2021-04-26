93a201d6e6235c68e87083bc04d54f579f8d5ebf
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-04-26 14:58:11 +0300

    show b lade edit bootons aded

--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -5,9 +5,12 @@
    <div class="row justify-content-center">
        <div class="col-md-8">
            <div class="card">
-               <div class="card-header"><h3>Book {{$book->title}}</h3> </div>
-               <div class="make-inline"> <h3> Description</h3>
-               {!!$book->short_description!!}
+               <div class="card-header"><h3>Book: {{$book->title}}</h3> </div>
+               <div class="make-inline"> <h4> Description:{!!$book->short_description!!}</h4>
+               </div>
+               <div class="make-inline-show">
+               <a href="{{route('author.edit',[$book->bookAuthor])}}" class="btn btn-info">EDIT AUTHOR</a>
+               <a href="{{route('book.edit',[$book])}}" class="btn btn-info">EDIT BOOK</a>
                </div>
                 
                </div>
